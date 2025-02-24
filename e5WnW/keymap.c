7e2e20f1f7787ebd01fa86f032c4c1d73b9a5e7d
Brendon McGay <dev@brendonn.com>
2025-02-24 15:19:58 -0500

    test

--- e5WnW/keymap.c
+++ e5WnW/keymap.c
@@ -230,5 +230,5 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
     'L','L','L','L','L','L','R','R','R','R','R','R',        
     'L','L','L','L','L','L','R','R','R','R','R','R',        
     'L','L','L','L','L','L','R','R','R','R','R','R',        
-    'L','L','L','L','L','*','R','R','R','R','R'        
+    'L','L','L','L','L','*','*','R','R','R','R','R'        
     );
