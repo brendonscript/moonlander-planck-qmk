00ad24332944b4df8f9a5d6b4933df1988d1524b
Brendon McGay <dev@brendonn.com>
2025-03-11 17:56:34 -0400

    windows layer leader

--- 5qJbe/keymap.c
+++ 5qJbe/keymap.c
@@ -372,19 +372,10 @@ bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
 //     // Do something when the leader key is pressed
 // }
 //
-// void leader_end_user(void) {
-//     if (leader_sequence_one_key(KC_F)) {
-//         // Leader, f => Types the below string
-//         SEND_STRING("QMK is awesome.");
-//     } else if (leader_sequence_two_keys(KC_D, KC_D)) {
-//         // Leader, d, d => Ctrl+A, Ctrl+C
-//         SEND_STRING(SS_LCTL("a") SS_LCTL("c"));
-//     } else if (leader_sequence_three_keys(KC_D, KC_D, KC_S)) {
-//         // Leader, d, d, s => Types the below string
-//         SEND_STRING("https://start.duckduckgo.com\n");
-//     } else if (leader_sequence_two_keys(KC_A, KC_S)) {
-//         // Leader, a, s => GUI+S
-//         tap_code16(LGUI(KC_S));
-//     }
-// }
+void leader_end_user(void) {
+    if (leader_sequence_one_key(KC_W)) {
+        // Enable Windows Layer
+        layer_on(1);
+    } 
+}
 
